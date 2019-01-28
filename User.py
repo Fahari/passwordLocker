2127de96e752a85313810e65b393c8991d1cdf52
Fahari <karonjekevin67@gmail.com>
2019-01-28 15:00:38 +0300

    add user class constructor instance variables

--- User.py
+++ User.py
@@ -12,3 +12,8 @@ def __init__(self,first_name,last_name,user_name,email):
             user_name: New contact phone number.
             email : New contact email address.
     '''
+
+    self.first_name = first_name
+    self.last_name = last_name
+    self.user_name = user_name
+    self.email = email
