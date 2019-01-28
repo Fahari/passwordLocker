7523236e9426dd86b8e0771329b75b62ad02dd36
Fahari <karonjekevin67@gmail.com>
2019-01-28 14:56:17 +0300

    create the init() method while also  creating constructor

--- User.py
+++ User.py
@@ -2,3 +2,13 @@ class User:
     """
     Class that generates new instances of User
     """
+def __init__(self,first_name,last_name,user_name,email):
+    '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            first_name: New contact first name.
+            last_name : New contact last name.
+            user_name: New contact phone number.
+            email : New contact email address.
+    '''
