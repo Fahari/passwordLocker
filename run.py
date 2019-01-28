e6c57bedf2f862c27618217496454c06db524d79
Fahari <karonjekevin67@gmail.com>
2019-01-29 00:23:45 +0300

    duplicated all user functions to credentials functions

--- run.py
+++ run.py
@@ -38,3 +38,44 @@ def display_user():
     Function that returns all the saved users
     '''
     return User.display_user()
+
+
+
+
+
+def create_credentials(username,email):
+    '''
+    Function to create a new credential
+    '''
+    new_credentials = Credentials(username,email)
+    return new_credentials
+
+def save_credentials(credentials):
+    '''
+    Function to save credentials
+    '''
+    credentials.save_credentials()
+
+def del_credentials(credentials):
+    '''
+    Function to delete a credential
+    '''
+    credentials.delete_credentials()
+
+def find_credentials(user_name):
+    '''
+    Function that finds a credential by user_name and returns the credential
+    '''
+    return Credentials.find_by_user_name(user_name)
+
+def check_existing_credentials(user_name):
+    '''
+    Function that check if a credential exists with that user_name and return a Boolean
+    '''
+    return Credentials.credential_exist(user_name)
+
+def display_credentials():
+    '''
+    Function that returns all the saved credentials
+    '''
+    return Credentials.display_credentials()
