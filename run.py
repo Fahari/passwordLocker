d883de3f40e9ee2dcfa40d2deee1122c00289e41
Fahari <karonjekevin67@gmail.com>
2019-01-29 00:03:54 +0300

    create a new user call function

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3.6
+from user import user
+from user import Credentials
+
+def create_user(fname,lname,username,email):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(fname,lname,phone,email)
+    return new_user
