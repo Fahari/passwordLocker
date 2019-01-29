14f5ea1705a05183c3f2f0d94b6dbdf2e694a502
Fahari <karonjekevin67@gmail.com>
2019-01-29 09:32:03 +0300

    a functioning application

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3.6
-from user import user
+from user import User
 from user import Credentials
 
 def create_user(fname,lname,username,email):
     '''
     Function to create a new user
     '''
-    new_user = User(fname,lname,phone,email)
+    new_user = User(fname,lname,username,email)
     return new_user
 
 def save_user(user):
@@ -79,3 +79,82 @@ def display_credentials():
     Function that returns all the saved credentials
     '''
     return Credentials.display_credentials()
+
+
+
+
+def main():
+    print("Hello Welcome to passwordLocker. What is your name?")
+    user_name = input()
+
+    print(f"Hello {user_name}. what would you like to do?")
+    print('\n')
+
+    while True:
+
+
+        print("Use these short codes : cc - create new credentials, dc - display credentials, fc -find credentials, ex -exit passwordLocker")
+
+        short_code = input().lower()
+
+        if short_code == 'cc':
+
+            print("New Credentials")
+            print("-"*10)
+
+            print ("First name ....")
+            f_name = input()
+
+            print("Last name ...")
+            l_name = input()
+
+            print("Username ...")
+            u_name = input()
+
+            print("Email address ...")
+            e_address = input()
+
+
+            save_user(create_user(f_name,l_name,u_name,e_address)) # create and save new contact.
+            print ('\n')
+            print(f"New Credentials {f_name} {l_name} created")
+            print ('\n')
+
+        elif short_code == 'dc':
+
+            if display_user():
+                    print("Here is a list of all your saved credentials")
+                    print('\n')
+
+                    for user in display_user():
+                            print(f"{user.first_name} {user.last_name} .....{user.user_name}")
+
+                    print('\n')
+            else:
+                    print('\n')
+                    print("You dont seem to have any credentials saved yet")
+                    print('\n')
+
+        elif short_code == 'fc':
+
+                print("Enter the username you want to search for")
+
+                search_number = input()
+
+                if check_existing_user(search_number):
+                        search_user = find_user(search_number)
+                        print(f"{search_user.first_name} {search_user.last_name}")
+                        print('-' * 20)
+
+                        # print(f"Phone number.......{search_contact.phone_number}")
+                        print(f"Email address.......{search_user.email}")
+                else:
+                        print("That credential does not exist")
+        elif short_code == "ex":
+                print("Bye .......")
+                break
+        else:
+                print("Please use the short codes")
+
+if __name__ == '__main__':
+    main()
